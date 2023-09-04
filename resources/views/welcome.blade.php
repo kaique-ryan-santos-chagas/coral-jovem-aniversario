192003df50bb0e09177a4fa1ff7ec525acb934d6
Kaique Ryan Santos Chagas <kaique.chagas@globalhitss.com.br>
2023-09-04 19:19:07 -0300

    Projeto para o aniversário do Coral Jovem 2023

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Righteous">
+
+        <title>Coral Jovem</title>
+    </head>
+
+    <body>
+
+        <center>
+
+            <h1 class="title">CORAL JOVEM</h1>
+
+            <div id="login">
+                
+                <h2 class="subtitle" id="enter">Entrar</h2>
+                
+                <label for="email" class="subtitle" id="label-email">E-mail</label>
+                <br>
+                <input id="email" class="input" autocomplete="false" />
+                
+                <br>
+
+                <label for="password" class="subtitle" id="label-password">Senha</label>
+                <br>
+                <input id="password" class="input" autocomplete="false" />
+
+            </div>
+
+        </center>
+
+    </body>
+
+</html>
